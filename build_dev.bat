230192725166c88c5763474ef98c7dd6fa0e390d
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-20 12:05:06 +0100

    Create build_dev.bat

---
+++ build_dev.bat
@@ -0,0 +1,25 @@
+@echo | call install_deps.bat
+
+SET MSBuildPTH=C:\Program Files\Microsoft Visual Studio\2022\Community
+IF EXIST "%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" GOTO buildm
+
+SET MSBuildPTH=D:\Program Files\Microsoft Visual Studio\2022\Community
+IF EXIST "%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" GOTO buildm
+
+SET MSBuildPTH=C:\Program Files\Microsoft Visual Studio\2022\Enterprise
+IF EXIST "%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" GOTO buildm
+
+SET MSBuildPTH=D:\Program Files\Microsoft Visual Studio\2022\Enterprise
+IF EXIST "%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" GOTO buildm
+
+
+echo MSVC's MSBuild executable not found!
+GOTO endc
+
+:buildm
+pushd msvc_project
+"%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" VapourSynth.sln /t:Clean;Build /p:Configuration=Release /p:Platform=x64
+"%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" VapourSynth.sln /t:Clean;Build /p:Configuration=Release /p:Platform=Win32
+popd
+
+@echo | call cython_build_dev.bat
